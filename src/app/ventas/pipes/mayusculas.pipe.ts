7c9e282a6df61b098c0fe9d170c07a556fd3b273
Rolando guerra <rolandoguerra1991@gmail.com>
2021-12-03 22:49:25 -0400

    mayusculas pipe

---
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,13 @@
+import {  Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mayusculas'
+})
+
+export class MayusculasPipe implements PipeTransform {
+
+    transform(value: string): string {
+      return value.toUpperCase();
+    }
+
+}
